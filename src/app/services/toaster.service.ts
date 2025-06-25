db48b0f5c3fe5d2225ac4346407118291102d58e
Rohit Kumar <rohitnarnolia88@gmail.com>
2025-06-25 21:16:24 +0530

    add toastr service

---
+++ src/app/services/toaster.service.ts
@@ -0,0 +1,26 @@
+// toaster.service.ts
+import { Injectable } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AppToasterService {
+  constructor(private toastr: ToastrService) {}
+
+  success(message: string, title: string = 'Success') {
+    this.toastr.success(`${message}`, title, { enableHtml: true });
+  }
+
+  error(message: string, title: string = 'Error') {
+    this.toastr.error(`${message}`, title, { enableHtml: true });
+  }
+
+  info(message: string, title: string = 'Info') {
+    this.toastr.info(`${message}`, title, { enableHtml: true });
+  }
+
+  warning(message: string, title: string = 'Warning') {
+    this.toastr.warning(`${message}`, title, { enableHtml: true });
+  }
+}
